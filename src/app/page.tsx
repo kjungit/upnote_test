c596e1155c4cb99a556bbc9b6aea10f8974e50f6
kjun <kjun_all@naver.com>
2023-12-07 21:44:56 +0900

    Feat: page 설정 - styles - NoteBooksSection 추가 - MemoSection 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import MemoSection from "@/components/MemoSection";
+import NoteBooksSection from "@/components/NoteBooksSection";
+
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <div className="h-screen border-x-2 border-black min-w-[1400px] max-w-[1920px] flex justify-center">
+      <NoteBooksSection />
+      <MemoSection />
+    </div>
   );
 }
